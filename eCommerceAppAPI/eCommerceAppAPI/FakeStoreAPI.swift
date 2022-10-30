d7adab8ebc2c8cf42d51522f5b5ee1a7fccfff16
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-10-30 15:34:40 +0300

    Moya library has been added to the eCommerceAppAPI module. FakeStoreAPI has been prepared with moya library.

---
+++ eCommerceAppAPI/eCommerceAppAPI/FakeStoreAPI.swift
@@ -0,0 +1,48 @@
+//
+//  FakeStoreAPI.swift
+//  eCommerceAppAPI
+//
+//  Created by GÜRHAN YUVARLAK on 30.10.2022.
+//
+
+import Foundation
+import Moya
+
+public let fakeStoreProvider = MoyaProvider<FakeStoreApi>()
+
+public enum FakeStoreApi {
+    case getProducts
+    case getSingleProduct(_ id: String)
+}
+
+extension FakeStoreApi: TargetType {
+    public var baseURL: URL {
+        guard let url = URL(string: "https://fakestoreapi.com/products") else {
+            fatalError("Base URL not found or not in correct format.")
+        }
+        return url
+    }
+    
+    public var path: String {
+        switch self {
+            case .getProducts:
+                return "/"
+            case .getSingleProduct(let id):
+                return "/\(id)"
+        }
+    }
+    
+    public var method: Moya.Method {
+        .get
+    }
+    
+    public var task: Moya.Task {
+        .requestPlain
+    }
+    
+    public var headers: [String : String]? {
+        nil
+    }
+    
+    
+}
