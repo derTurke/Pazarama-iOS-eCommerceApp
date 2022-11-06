a3c94c424489f4c5ed29c674050ba6b7b681b507
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-11-06 04:41:48 +0300

    Onboarding View has been updated and VIPER design pattern has been added.

--- eCommerceApp/eCommerceApp/Screens/Onboarding/OnboardingContract.swift
+++ eCommerceApp/eCommerceApp/Screens/Onboarding/OnboardingContract.swift
@@ -5,4 +5,56 @@
 //  Created by GÜRHAN YUVARLAK on 5.11.2022.
 //
 
-import Foundation
+import UIKit
+
+// MARK: View Output (Presenter -> View)
+protocol PresenterToViewOnboardingProtocol: AnyObject {
+    func didChangePageNumber(_ currentPageNumber: Int)
+    func didUpdateContentOffset(_ offset: CGPoint)
+    func didAddOnboardingView(_ onboardingView: [CustomOnboardingView])
+}
+
+// MARK: View Input (View -> Presenter)
+protocol ViewToPresenterOnboardingProtocol {
+    var view: PresenterToViewOnboardingProtocol? { get set }
+    var interactor: PresenterToInteractorOnboardingProtocol? { get set }
+    var router: PresenterToRouterOnboardingProtocol? { get set }
+    
+    var currentPageNumber: Int { get set}
+    var onboardingView: [CustomOnboardingView]? { get set }
+    var contentOffset: CGPoint? { get set }
+    
+    func updateScrollViewContentOffset(with pageNumber: Int)
+    func didTapNextButton(pageNumber: Int)
+    func didTapPrevButton(pageNumber: Int)
+    func didTapSkipButton()
+    func setupScrollViewDidEndDecelerating(_ scrollView: UIScrollView)
+    func addOnboardingView()
+}
+
+// MARK: Interactor Input (Presenter -> Interactor)
+protocol PresenterToInteractorOnboardingProtocol {
+    var presenter: InteractorToPresenterOnboardingProtocol? { get set }
+    
+    func createOnboardingView()
+    func setUserDefaults()
+}
+
+// MARK: Interactor Output (Interactor -> Presenter)
+protocol InteractorToPresenterOnboardingProtocol: AnyObject {
+    func didCreateOnboardingView(_ onboardingView: [CustomOnboardingView]?)
+    func didSetUserDefaults()
+}
+
+// MARK: Router Input (Presenter -> Router)
+protocol PresenterToRouterOnboardingProtocol {
+    static func createModule() -> UIViewController
+    func pushToAuth(on view: PresenterToViewOnboardingProtocol)
+}
+
+protocol OnboardingViewDelegate: AnyObject {
+    func updateScrollViewContentOffset(with pageNumber: Int)
+    func didTapNextButton(pageNumber: Int)
+    func didTapPrevButton(pageNumber: Int)
+    func didTapSkipButton()
+}
--- eCommerceApp/eCommerceApp/Screens/Onboarding/OnboardingViewController.swift
+++ eCommerceApp/eCommerceApp/Screens/Onboarding/OnboardingViewController.swift
@@ -6,121 +6,60 @@
 //
 
 import UIKit
-protocol OnboardingViewDelegate: AnyObject {
-    func didTapNextButton()
-    func didTapPrevButton()
-    func didTapSkipButton()
-}
-
 final class OnboardingViewController: UIViewController {
     //MARK: - Properties
-    private let onboardingView = OnboardingView()
-    private let pageWidth: CGFloat = UIScreen.main.bounds.width
-    private var currentPageNumber: Int = .zero {
-        didSet {
-            if currentPageNumber == onboardingViews.count - 1 {
-                onboardingView.nextButton.setTitle("Done", for: .normal)
-            } else {
-                onboardingView.nextButton.setTitle("Next", for: .normal)
-            }
-            
-            if currentPageNumber == .zero {
-                onboardingView.prevButton.isHidden = true
-            } else {
-                onboardingView.prevButton.isHidden = false
-            }
-            
-            onboardingView.pageControl.currentPage = currentPageNumber
-            updateScrollViewContentOffset(with: currentPageNumber)
-        }
-    }
-    private var onboardingViews = [CustomOnboardingView]() {
-        didSet {
-            let numberOfPages = onboardingViews.count
-            onboardingView.scrollView.contentSize.width = CGFloat(numberOfPages) * pageWidth
-            onboardingView.pageControl.numberOfPages = numberOfPages
-            guard let onboarding = onboardingViews.last else {
-                fatalError("OnboardingView not found.")
-            }
-            onboardingView.contentView.addSubview(onboarding)
-            onboarding.translatesAutoresizingMaskIntoConstraints = false
-            
-            if onboardingViews.count == 1 {
-                NSLayoutConstraint.activate([
-                    onboarding.leadingAnchor.constraint(equalTo: onboardingView.contentView.leadingAnchor),
-                    onboarding.topAnchor.constraint(equalTo: onboardingView.contentView.topAnchor),
-                    onboarding.bottomAnchor.constraint(equalTo: onboardingView.contentView.bottomAnchor),
-                    onboarding.widthAnchor.constraint(equalToConstant: pageWidth)
-                ])
-            } else {
-                NSLayoutConstraint.activate([
-                    onboarding.leadingAnchor.constraint(equalTo: onboardingViews.first!.trailingAnchor),
-                    onboarding.topAnchor.constraint(equalTo: onboardingView.contentView.topAnchor),
-                    onboarding.bottomAnchor.constraint(equalTo: onboardingView.contentView.bottomAnchor),
-                    onboarding.widthAnchor.constraint(equalToConstant: pageWidth),
-                    onboarding.trailingAnchor.constraint(equalTo: onboardingView.contentView.trailingAnchor)
-                ])
-            }
-        }
-    }
+    private let customView = OnboardingView()
+    var presenter: ViewToPresenterOnboardingProtocol!
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = onboardingView
-        onboardingView.delegate = self
-        onboardingView.setScrollViewDelegate(self)
         navigationItem.hidesBackButton = true
-        
-        let firstOnboardingView = CustomOnboardingView()
-        firstOnboardingView.image = UIImage(named: "onboard-1")
-        firstOnboardingView.text = "Güvenilir ve kullanıcı dostu olan pazaryeri uygulamasına hoşgeldiniz."
-        onboardingViews.append(firstOnboardingView)
-        
-        let secondOnboardingView = CustomOnboardingView()
-        secondOnboardingView.image = UIImage(named: "onboard-2")
-        secondOnboardingView.text = "Siparişlerinizi hızlıca verebilmeniz için ışık hızında bir pazaryeri olma yolunda ilerlemeye ve gelişmeye devam ediyoruz."
-        onboardingViews.append(secondOnboardingView)
-        
-    }
-    //MARK: - Methods
-    private func goAuth() {
-        navigationController?.pushViewController(AuthRouter.createModule(), animated: true)
-        setSkipOnboarding()
-    }
-    
-    private func updateScrollViewContentOffset(with pageNumber: Int) {
-        let contentOffset = CGPoint(x: pageWidth * CGFloat(pageNumber), y: .zero)
-        onboardingView.scrollView.setContentOffset(contentOffset, animated: true)
-    }
-    
-    private func setSkipOnboarding() {
-        UserDefaults.standard.set(1, forKey: "skipOnboarding")
+        view = customView
+        customView.delegate = self
+        customView.scrollViewDelegate(self)
+        presenter.addOnboardingView()
     }
 }
 
+//MARK: - UIScrollViewDelegate
 extension OnboardingViewController: UIScrollViewDelegate {
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let currentPage = Int(onboardingView.scrollView.contentOffset.x / pageWidth)
-        currentPageNumber = currentPage
+        presenter.setupScrollViewDidEndDecelerating(scrollView)
     }
 }
 
+//MARK: - ViewDelegate
 extension OnboardingViewController: OnboardingViewDelegate {
-    func didTapNextButton() {
-        if currentPageNumber < onboardingViews.count - 1 {
-            currentPageNumber += 1
-        } else {
-            goAuth()
-        }
+    func updateScrollViewContentOffset(with pageNumber: Int) {
+        presenter.updateScrollViewContentOffset(with: pageNumber)
     }
     
-    func didTapPrevButton() {
-        if currentPageNumber > 0 {
-            currentPageNumber -= 1
-        }
+    func didTapNextButton(pageNumber: Int) {
+        presenter.didTapNextButton(pageNumber: pageNumber)
+    }
+    
+    func didTapPrevButton(pageNumber: Int) {
+        presenter.didTapPrevButton(pageNumber: pageNumber)
     }
     
     func didTapSkipButton() {
-        goAuth()
+        presenter.didTapSkipButton()
     }
+}
+
+//MARK: - PresenterToViewOnboardingProtocol
+extension OnboardingViewController: PresenterToViewOnboardingProtocol {
+    func didAddOnboardingView(_ onboardingView: [CustomOnboardingView]) {
+        customView.customView = onboardingView
+    }
+    
+    func didChangePageNumber(_ currentPageNumber: Int) {
+        customView.currentPageNumber = currentPageNumber
+    }
+    
+    func didUpdateContentOffset(_ offset: CGPoint) {
+        customView.setupContentOffset(offset)
+    }
+    
+    
 }
